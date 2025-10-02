a720d2c04da6ab836127fcfe68db498c89a19f0e
Thuyết Bảo <trthuyetbao@gmail.com>
2025-10-02 14:30:06 +0700

    [Fix] Fixed deployment when build with `uv` (#2)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## v0.1.2 (2025-10-02)
+
+- [x] (Fix) Fixed build and test related to isolation installment with `uv`
+
+- [x] Updated cli print result
+
 ## v0.1.1 (2025-09-30)
 
 - [x] Added MIT License
--- einvoice_lens/cli.py
+++ einvoice_lens/cli.py
@@ -1,17 +1,37 @@
 #!/bin/python3
 
-# Internal
+# Global
+import pprint
 import argparse
+import textwrap
+
+# Internal
 from einvoice_lens.engine import parse_commerical_invoice
 
 
 if __name__ == "__main__":
 
     # Handlers
-    parser = argparse.ArgumentParser(description="Einvoice Lens CLI")
-    parser.add_argument("path", help="Path to the PDF file", type=str)
-    args = parser.parse_args()
+    parser = argparse.ArgumentParser(
+        prog="python -m einvoice_lens.cli",
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=textwrap.dedent("""
+        [Einvoice Lens] CLI
+
+        Usage
+        -----
+
+        Base case
+        >>> python -m einvoice_lens.cli --path <document-path>
+
+        Help
+        >>> python -m einvoice_lens.cli --help
+        """),
+        epilog="Copyright (c) of Thuyet Bao"
+    )
+    parser.add_argument("--path", help="Path to the PDF file", type=str)
+    parameters = parser.parse_args()
 
     # Parse
-    result = parse_commerical_invoice(args.path)
-    print(result)
+    result = parse_commerical_invoice(parameters.path)
+    pprint.pp(result, depth=4)
